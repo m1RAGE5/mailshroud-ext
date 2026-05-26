a07af2b7cb13927641240cf3ad3738c660560e0f
Illia M. <illia.masakov@gmail.com>
2026-05-26 17:09:26 +0300

    chore: reorganize base of the project

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'wxt';
+import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  srcDir: 'src',
-  modules: ['@wxt-dev/module-svelte'],
+  srcDir: "src",
+  modules: ["@wxt-dev/module-svelte"],
+  manifest: {
+    permissions: ["storage", "activeTab"],
+    host_permissions: ["*://mail.google.com/*", "*://gmail.com/*"],
+    web_accessible_resources: [
+      {
+        resources: ["injected.js"],
+        matches: ["*://mail.google.com/*", "*://gmail.com/*"],
+      },
+    ],
+  },
 });
